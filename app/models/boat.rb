c77a8244171c92286aff9449b719f71f9babc743
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-12-05 20:09:46 -0700

    validations

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,6 +2,9 @@ class Boat < ApplicationRecord
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   
+  validates :name, :uniqueness => true
+  validates_inclusion_of :location, in: ['USA', 'Russia', 'China', 'Thialand']
+
   belongs_to :user
  	has_many :boat_jobs
  	has_many :jobs, through: :boat_jobs, dependent: :destroy
