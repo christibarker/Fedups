1e3ec82453dbabf4ae6fb4a8388c14315e6b3232
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-12-06 09:32:10 -0700

    updated boats table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206162728) do
+ActiveRecord::Schema.define(version: 20171206163029) do
 
   create_table "boat_jobs", force: :cascade do |t|
     t.integer "boat_id"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20171206162728) do
   create_table "boats", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.string "max_capacity"
+    t.integer "max_capacity"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
